b2a19aeb8135a80e3ebf25d99ed5c49430058f65
Adam Dernis <avid29@live.com>
2021-10-03 03:05:22 -0400

    Created Shader Material

--- src/RenderSharp.RayTracing/RenderSharp.RayTracing.Hlsl/Materials/Material.cs
+++ src/RenderSharp.RayTracing/RenderSharp.RayTracing.Hlsl/Materials/Material.cs
@@ -1,7 +1,27 @@
-﻿namespace RenderSharp.RayTracing.HLSL.Materials
+﻿using ComputeSharp;
+using RenderSharp.RayTracing.HLSL.Rays;
+using RenderSharp.RayTracing.HLSL.Utils;
+
+namespace RenderSharp.RayTracing.HLSL.Materials
 {
     public struct Material
     {
-        // TODO: Material scatter and emissions
+        public Float4 albedo;
+
+        public static Material Create(Float4 albedo)
+        {
+            Material material;
+            material.albedo = albedo;
+            return material;
+        }
+
+        public static void Scatter(Material material, Ray ray, RayCast cast, ref uint randState, out Float4 attenuation, out Ray scatter)
+        {
+            Float3 target = cast.origin + cast.normal + RandUtils.RandomInUnitSphere(ref randState);
+            attenuation = material.albedo;
+            scatter = Ray.Create(cast.origin, target - cast.origin);
+        }
+
+        // TODO: Material emissions
     }
 }
