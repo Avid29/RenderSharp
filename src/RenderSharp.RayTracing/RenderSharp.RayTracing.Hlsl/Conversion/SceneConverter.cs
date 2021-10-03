3d9c5892fa168fd0d0a61bb6c317cd064e8d5a23
Adam Dernis <avid29@live.com>
2021-10-03 03:04:49 -0400

    Added Common material to shader material conversion

--- src/RenderSharp.RayTracing/RenderSharp.RayTracing.Hlsl/Conversion/SceneConverter.cs
+++ src/RenderSharp.RayTracing/RenderSharp.RayTracing.Hlsl/Conversion/SceneConverter.cs
@@ -1,4 +1,7 @@
 ﻿using ComputeSharp;
+using RenderSharp.Common.Materials;
+using System.Collections.Generic;
+
 using CommonCamera = RenderSharp.Common.Components.Camera;
 using CommonScene = RenderSharp.Common.Components.Scene;
 using CommonSky = RenderSharp.Common.Skys.Sky;
@@ -9,14 +12,18 @@ using ShaderScene = RenderSharp.RayTracing.HLSL.Components.Scene;
 using ShaderSky = RenderSharp.RayTracing.HLSL.Skys.Sky;
 using ShaderSphere = RenderSharp.RayTracing.HLSL.Geometry.Sphere;
 using ShaderWorld = RenderSharp.RayTracing.HLSL.Components.World;
+using ShaderMaterial = RenderSharp.RayTracing.HLSL.Materials.Material;
 
 namespace RenderSharp.RayTracing.HLSL.Conversion
 {
     public class SceneConverter
     {
         private GraphicsDevice _gpu;
+        private Dictionary<DiffuseMaterial, int> _materialMap = new Dictionary<DiffuseMaterial, int>();
         private ReadOnlyBuffer<ShaderSphere> _geometryBuffer;
+        private ReadOnlyBuffer<ShaderMaterial> _materialBuffer;
         private bool _isGeometryLoaded;
+        private bool _areMaterialsLoaded;
 
         public SceneConverter(GraphicsDevice gpu)
         {
@@ -26,7 +33,11 @@ namespace RenderSharp.RayTracing.HLSL.Conversion
 
         public bool IsGeomertyLoaded => _isGeometryLoaded;
 
+        public bool AreMaterialsLoaded => _areMaterialsLoaded;
+
         public ReadOnlyBuffer<ShaderSphere> GeometryBuffer => _geometryBuffer;
+
+        public ReadOnlyBuffer<ShaderMaterial> MaterialBuffer => _materialBuffer;
 
         public ShaderScene ConvertScene(CommonScene scene)
         {
@@ -37,6 +48,8 @@ namespace RenderSharp.RayTracing.HLSL.Conversion
             // Default config for now
             output.config.samples = 16;
             output.config.maxBounces = 16;
+
+            FinishMaterialLoading();
 
             return output;
         }
@@ -70,22 +83,57 @@ namespace RenderSharp.RayTracing.HLSL.Conversion
             return ShaderCamera.CreateCamera(camera.Origin, camera.FocalLength, camera.FOV);
         }
 
-        public void LoadGeometry(ShaderSphere[] geometry)
-        {
-            if (_isGeometryLoaded)
-                _geometryBuffer.Dispose();
-
-            _isGeometryLoaded = true;
-
-            _geometryBuffer = _gpu.AllocateReadOnlyBuffer(geometry);
-        }
-
         public ShaderSphere ConvertSphere(CommonSphere sphere)
         {
             ShaderSphere output;
             output.center = sphere.Center;
             output.radius = sphere.Radius;
+            
+            if (_materialMap.ContainsKey(sphere.Material))
+            {
+                output.matId = _materialMap[sphere.Material];
+            } else
+            {
+                int id = _materialMap.Count;
+                _materialMap.Add(sphere.Material, id);
+                output.matId = id;
+            }
+
             return output;
+        }
+
+        public ShaderMaterial ConvertMaterial(DiffuseMaterial material)
+        {
+            ShaderMaterial output;
+            output.albedo = material.Albedo;
+            return output;
+        }
+
+        public void LoadGeometry(ShaderSphere[] geometry)
+        {
+            if (_isGeometryLoaded)
+                _geometryBuffer.Dispose();
+
+            _geometryBuffer = _gpu.AllocateReadOnlyBuffer(geometry);
+            _isGeometryLoaded = true;
+        }
+
+        public void FinishMaterialLoading()
+        {
+            if (_areMaterialsLoaded)
+                _materialBuffer.Dispose();
+
+            ShaderMaterial[] materials = new ShaderMaterial[_materialMap.Count];
+
+            int i = 0;
+            foreach (var material in _materialMap.Keys)
+            {
+                materials[i] = ConvertMaterial(material);
+                i++;
+            }
+
+            _materialBuffer = _gpu.AllocateReadOnlyBuffer(materials);
+            _areMaterialsLoaded = true;
         }
     }
 }
