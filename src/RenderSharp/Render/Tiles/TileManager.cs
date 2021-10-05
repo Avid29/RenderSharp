b83b9f64beafb9c40bba25363db95fe7a6251d7c
Adam Dernis <avid29@live.com>
2021-10-05 17:51:32 -0400

    Created TileManaging system

---
+++ src/RenderSharp/Render/Tiles/TileManager.cs
@@ -0,0 +1,88 @@
+﻿using RenderSharp.Common.Render.Tiles;
+using RenderSharp.Render.Tiles;
+
+namespace RenderSharp.Render
+{
+    public class TileManager
+    {
+        private TileConfig _config;
+        private int _imageWidth;
+        private int _imageHeight;
+
+        public TileManager(int imageWidth, int imageHeight, TileConfig config)
+        {
+            _imageWidth = imageWidth;
+            _imageHeight = imageHeight;
+            _config = config;
+            PreviousTileIndex = 0;
+            TileCount = DetermineTileCount();
+        }
+
+        public int PreviousTileIndex { get; private set; }
+
+        public int TileCount { get; }
+
+        public bool Finished => !(PreviousTileIndex < TileCount);
+
+        public Tile GetNextTile()
+        {
+            if (PreviousTileIndex >= TileCount - 1)
+                return new Tile(0, 0, 0, 0); // TODO: Throw 
+
+            // TODO: More ordering options
+            PreviousTileIndex++;
+            return NextTileFromSide(_config.Order);
+        }
+
+        private int DetermineTileCount()
+        {
+            // TODO: More ordering options
+            return GetTileCountFromSide();
+        }
+
+        private Tile NextTileFromSide(TileOrder order)
+        {
+            int offsetX = _config.TileWidth;
+            int offsetY = _config.TileHeight;
+
+            // Round up
+            int perRow = (_imageWidth + offsetX - 1) / offsetX;
+
+            int row = PreviousTileIndex / perRow;
+            int col = PreviousTileIndex % perRow;
+
+            if (order == TileOrder.LeftRight)
+            {
+                int swap = row;
+                row = col;
+                col = swap;
+            }
+
+            int x = row * offsetX;
+            int y = col * offsetY;
+
+            int width = offsetX;
+            if (x + width > _imageWidth) width = _imageWidth - x;
+
+            int height = offsetY;
+            if (y + height > _imageWidth) height = _imageWidth - y;
+
+            return new Tile(x, y, width, height);
+        }
+
+        private int GetTileCountFromSide()
+        {
+            // X/Y, Row/Column changed to ambiguous U/V and Span
+            // This is because the distinction is not important
+
+            int offsetU = _config.TileWidth;
+            int offsetV = _config.TileHeight;
+
+            // Round up
+            int perUSpan = (_imageWidth + offsetU - 1) / offsetU;
+            int perVSpan = (_imageHeight + offsetV - 1) / offsetV;
+
+            return perUSpan * perVSpan;
+        }
+    }
+}
