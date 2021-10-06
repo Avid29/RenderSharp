8f87236b5181cd57b0ed1a4ca135c3985550f53e
Adam Dernis <avid29@live.com>
2021-10-05 23:11:36 -0400

    Created abstract buffers for CPU or GPU

---
+++ src/RenderSharp.Common/Devices/Buffers/IReadWriteImageBuffer.cs
@@ -0,0 +1,13 @@
+﻿using ComputeSharp;
+
+namespace RenderSharp.Common.Devices.Buffers
+{
+    public interface IReadWriteImageBuffer
+    {
+        int Width { get; }
+
+        int Height { get; }
+
+        public void CopyToGPU(ReadWriteTexture2D<Float4> output);
+    }
+}
