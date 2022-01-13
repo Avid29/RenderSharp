0f37a2634847142e4f6258d083ef7f941f4a96e3
Adam Dernis <avid29@live.com>
2022-01-13 02:55:04 -0500

    Added base shaders

---
+++ src/RenderSharp.RayTracing/GPU/Shaders/CameraCastShader.cs
@@ -0,0 +1,39 @@
+﻿using ComputeSharp;
+using RenderSharp.RayTracing.Scenes;
+using RenderSharp.RayTracing.Scenes.Cameras;
+using RenderSharp.RayTracing.Scenes.Rays;
+using RenderSharp.RayTracing.Utils;
+
+namespace RenderSharp.RayTracing.GPU.Shaders
+{
+    /// <summary>
+    /// A shader that genereates the intial rays from a Camera for a tile.
+    /// </summary>
+    [AutoConstructor]
+    [EmbeddedBytecode(DispatchAxis.XY)]
+    public partial struct CameraCastShader : IComputeShader
+    {
+        private readonly Scene scene;
+        private readonly FullCamera camera;
+        private readonly int2 offset;
+        private readonly int2 fullSize;
+        private readonly ReadWriteBuffer<Ray> rayBuffer;
+
+        public void Execute()
+        {
+            int2 pos = ThreadIds.XY;
+            int2 dis = DispatchSize.XY;
+            int bPos = pos.X * dis.X + pos.Y;
+
+            int x = offset.X + ThreadIds.X;
+            int y = offset.Y + ThreadIds.Y;
+            int s = scene.config.samples;
+            uint randState = (uint)(x * 1973 + y * 9277 + s * 26699) | 1;
+
+            float u = (x + RandUtils.RandomFloat(ref randState) / fullSize.X);
+            float v = 1 - ((y + RandUtils.RandomFloat(ref randState)) / fullSize.Y);
+            Ray ray = FullCamera.CreateRay(camera, u, v, ref randState);
+            rayBuffer[bPos] = ray;
+        }
+    }
+}
---
+++ src/RenderSharp.RayTracing/GPU/Shaders/InitializeShader.cs
@@ -0,0 +1,27 @@
+﻿using ComputeSharp;
+using RenderSharp.RayTracing.Scenes;
+
+namespace RenderSharp.RayTracing.GPU.Shaders
+{
+    [AutoConstructor]
+    [EmbeddedBytecode(DispatchAxis.XY)]
+    public partial struct InitalizeShader : IComputeShader
+    {
+        private readonly Scene scene;
+        private readonly int2 offset;
+
+        private readonly ReadWriteTexture2D<float4> attenuationBuffer;
+        private readonly ReadWriteTexture2D<uint> randStates;
+
+        public void Execute()
+        {
+            int2 pos = ThreadIds.XY;
+            int x = offset.X + ThreadIds.X;
+            int y = offset.Y + ThreadIds.Y;
+            int s = scene.config.samples;
+
+            attenuationBuffer[pos] = float4.One;
+            randStates[pos] = (uint)(x * 1973 + y * 9277 + s * 26699) | 1;
+        }
+    }
+}
