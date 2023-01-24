d7fcddb0fbe5be2fa40b39fdbaad30db9f3b38db
Adam Dernis <avid29@live.com>
2023-01-24 13:55:32 -0500

    Added project layout

---
+++ samples/RenderSharp.Samples.WinUI/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace RenderSharp.Samples.WinUI
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private Window _mainWindow;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            _mainWindow = new MainWindow();
+            _mainWindow.Activate();
+        }
+    }
+}
