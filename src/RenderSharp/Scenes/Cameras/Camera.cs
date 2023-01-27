2ea193327247ccb853776a64135ab0d2f2b884f8
Adam Dernis <avid29@live.com>
2023-01-27 16:15:13 -0500

    Added EulerXYZ and LookAt Create methods for common camera.

--- src/RenderSharp/Scenes/Cameras/Camera.cs
+++ src/RenderSharp/Scenes/Cameras/Camera.cs
@@ -1,16 +1,15 @@
 ﻿// Adam Dernis 2023
 
 using RenderSharp.Scenes.Geometry;
+using System;
 using System.Numerics;
 
 namespace RenderSharp.Scenes.Cameras;
 
 public class Camera : Object
 {
-    public Camera(Vector3 origin, Vector3 eulerXYZ, float fov)
+    public Camera(Vector3 origin, Quaternion rotation, float fov)
     {
-        var rotation = Quaternion.CreateFromYawPitchRoll(eulerXYZ.Y.ToRadians(), eulerXYZ.X.ToRadians(), eulerXYZ.Z.ToRadians());
-
         Transformation = new Transformation
         {
             Translation = origin,
@@ -18,6 +17,21 @@ public class Camera : Object
         };
 
         Fov = fov;
+    }
+    public static Camera CreateFromEuler(Vector3 origin, Vector3 eulerXYZ, float fov)
+    {
+        var rotation = Quaternion.CreateFromYawPitchRoll(eulerXYZ.Y.ToRadians(), eulerXYZ.X.ToRadians(), eulerXYZ.Z.ToRadians());
+        return new Camera(origin, rotation, fov);
+    }
+
+    public static Camera CreateFromLookAt(Vector3 origin, Vector3 lookAt, float fov)
+    {
+        var direction = Vector3.Normalize(lookAt - origin);
+        var axis = Vector3.Normalize(Vector3.Cross(Vector3.UnitZ, direction));
+        var angle = MathF.Acos(Vector3.Dot(Vector3.UnitZ, direction));
+        var rotation = Quaternion.CreateFromAxisAngle(axis, angle);
+
+        return new Camera(origin, rotation, fov);
     }
 
     public float Fov { get; set; }
