0244aa415b2b6dc4049d5097f94b3ff6cf970686
Adam Dernis <Avid29@Live.com>
2023-01-27 20:56:32 -0500

    Added Plane tessallation

---
+++ src/RenderSharp/Scenes/Geometry/Tessellation/Shapes/Plane.cs
@@ -0,0 +1,51 @@
+﻿// Adam Dernis 2023
+
+using RenderSharp.Scenes.Geometry.Meshes;
+using System;
+using System.Linq;
+using System.Numerics;
+
+namespace RenderSharp.Scenes.Geometry.Tessellation.Shapes;
+public class Plane : TessellatedShape
+{
+    public Plane()
+    {
+        Size = 2;
+    }
+
+    public float Size { get; set; }
+
+    public override Mesh ConvertToMesh()
+    {
+        Span<Vector3> vvs = stackalloc[]
+        {
+            new Vector3(-1, 0, -1),
+            new Vector3(-1, 0, 1),
+            new Vector3(1, 0, -1),
+            new Vector3(1, 0, 1),
+        };
+        
+        // Transform vertex vectors and make vertices
+        var vs = new Vertex[vvs.Length];
+        for (int i = 0; i < vvs.Length; i++)
+        {
+            ref var vertex = ref vvs[i];
+            vertex *= Size / 2;
+
+            // TODO: Vertex normals
+            vs[i] = new Vertex(vertex);
+        }
+
+        var faces = new[]
+        {
+            new Face(vs[0], vs[1], vs[2]),
+            new Face(vs[3], vs[1], vs[2]),
+        };
+
+        return new Mesh
+        {
+            Vertices = vs.ToList(),
+            Faces = faces.ToList(),
+        };
+    }
+}
