ad59f45ee3ead3e1cc115b877227dc99046ac000
Adam Dernis <Avid29@Live.com>
2023-03-09 11:25:07 -0500

    Radial gradient phong material

---
+++ src/Renderers/RenderSharp.RayTracing/Models/Materials/RadialGradientPhongMaterial.cs
@@ -0,0 +1,26 @@
+﻿// Adam Dernis 2023
+
+using System.Numerics;
+
+namespace RenderSharp.RayTracing.Models.Materials;
+
+public struct RadialGradientPhongMaterial
+{
+    public float4 diffuse0;
+    public float4 diffuse1;
+    public float4 specular;
+    public float4 ambient;
+    public float roughness;
+    public float scale;
+
+    public RadialGradientPhongMaterial(Vector3 diffuse0, Vector3 diffuse1, Vector3 specular, Vector3 ambient, float roughness, float scale,
+        float cDiffuse0 = 1, float cDiffuse1 = 1, float cSpecular = 1, float cAmbient = 1)
+    {
+        this.diffuse0 = new float4(diffuse0 * cDiffuse0, 1);
+        this.diffuse1 = new float4(diffuse1 * cDiffuse1, 1);
+        this.specular = new float4(specular * cSpecular, 1);
+        this.ambient = new float4(ambient * cAmbient, 1);
+        this.scale = scale;
+        this.roughness = roughness;
+    }
+}
---
+++ src/Renderers/RenderSharp.RayTracing/Shaders/Shading/Stock/MaterialShaders/RadialGradientPhongShader.cs
@@ -0,0 +1,84 @@
+﻿// Adam Dernis 2023
+
+using ComputeSharp;
+using RenderSharp.RayTracing.Models.Lighting;
+using RenderSharp.RayTracing.Models.Materials;
+using RenderSharp.RayTracing.Models.Rays;
+using RenderSharp.RayTracing.Shaders.Shading.Interfaces;
+
+namespace RenderSharp.RayTracing.Shaders.Shading.Stock.MaterialShaders;
+
+[EmbeddedBytecode(DispatchAxis.XY)]
+public partial struct RadialGradientPhongShader : IMaterialShader
+{
+    private readonly int matId;
+    private readonly RadialGradientPhongMaterial material;
+    
+#nullable disable
+    private ReadOnlyBuffer<Light> lightsBuffer;
+    private ReadWriteBuffer<Ray> rayBuffer;
+    private ReadWriteBuffer<Ray> shadowCastBuffer;
+    private ReadWriteBuffer<GeometryCollision> rayCastBuffer;
+    private IReadWriteNormalizedTexture2D<float4> colorBuffer;
+#nullable restore
+    
+    public RadialGradientPhongShader(int matId, RadialGradientPhongMaterial material)
+    {
+        this.matId = matId;
+        this.material = material;
+    }
+
+    /// <inheritdoc/>
+    public void Execute()
+    {
+        // Get the index of resources managed by the current thread
+        // in both 2D textures and flat buffers
+        int2 index2D = ThreadIds.XY;
+        int fIndex = (index2D.Y * DispatchSize.X) + index2D.X;
+        
+        var cast = rayCastBuffer[fIndex];
+        var ray = rayBuffer[fIndex];
+
+        // If this material was not hit do not execute
+        if (cast.matId != matId)
+            return;
+
+        // Calculate diffuse and specular intensity
+        float4 diffuseIntensity = float4.Zero;
+        float4 specularIntensity = float4.Zero;
+        for (int i = 0; i < lightsBuffer.Length; i++)
+        {
+            var fShadowIndex = (i * DispatchSize.X * DispatchSize.Y) + (index2D.Y * DispatchSize.X) + index2D.X; 
+            var l = shadowCastBuffer[fShadowIndex].direction;
+            if (Hlsl.Length(l) == 0)
+                continue;
+
+            var n = cast.smoothNormal;
+            var v = ray.direction;
+            var h = Hlsl.Normalize(l - v);
+
+            diffuseIntensity += lightsBuffer[i].color * Hlsl.Max(Hlsl.Dot(n, l), 0f);
+            specularIntensity += lightsBuffer[i].color * Hlsl.Pow(Hlsl.Dot(n, h), material.roughness);
+        }
+
+        var x = Hlsl.Clamp(Hlsl.Length(cast.position) / material.scale, 0f, 1f);
+        var diffuse = (material.diffuse0 * x) + (material.diffuse1 * (1 - x));
+
+        // Sum ambient, diffuse, and specular components
+        colorBuffer[index2D] += material.ambient;
+        colorBuffer[index2D] += diffuse * diffuseIntensity;
+        colorBuffer[index2D] += material.specular * specularIntensity;
+    }
+
+    ReadOnlyBuffer<Light> IMaterialShader.LightBuffer  { set => lightsBuffer = value; }
+
+    ReadWriteBuffer<Ray> IMaterialShader.RayBuffer { set => rayBuffer = value; }
+
+    ReadWriteBuffer<Ray> IMaterialShader.ShadowCastBuffer { set => shadowCastBuffer = value; }
+
+    ReadWriteBuffer<GeometryCollision> IMaterialShader.RayCastBuffer {  set => rayCastBuffer = value; }
+
+    IReadWriteNormalizedTexture2D<float4> IMaterialShader.AttenuationBuffer { set => _ = value; }
+
+    IReadWriteNormalizedTexture2D<float4> IMaterialShader.ColorBuffer { set => colorBuffer = value; }
+}
