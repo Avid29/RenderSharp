eb563956a5aa2d96e6c434b59cb57dd939a67b73
Adam Dernis <Avid29@Live.com>
2023-03-09 11:24:57 -0500

    Checkered phong material

---
+++ src/Renderers/RenderSharp.RayTracing/Shaders/Shading/Stock/MaterialShaders/CheckeredPhongShader.cs
@@ -0,0 +1,86 @@
+﻿// Adam Dernis 2023
+
+using ComputeSharp;
+using RenderSharp.RayTracing.Models.Lighting;
+using RenderSharp.RayTracing.Models.Materials;
+using RenderSharp.RayTracing.Models.Rays;
+using RenderSharp.RayTracing.Shaders.Shading.Interfaces;
+
+namespace RenderSharp.RayTracing.Shaders.Shading.Stock.MaterialShaders;
+
+[EmbeddedBytecode(DispatchAxis.XY)]
+public partial struct CheckeredPhongShader : IMaterialShader
+{
+    private readonly int matId;
+    private readonly CheckeredPhongMaterial material;
+    
+#nullable disable
+    private ReadOnlyBuffer<Light> lightsBuffer;
+    private ReadWriteBuffer<Ray> rayBuffer;
+    private ReadWriteBuffer<Ray> shadowCastBuffer;
+    private ReadWriteBuffer<GeometryCollision> rayCastBuffer;
+    private IReadWriteNormalizedTexture2D<float4> colorBuffer;
+#nullable restore
+
+    public CheckeredPhongShader(int matId, CheckeredPhongMaterial material)
+    {
+        this.matId = matId;
+        this.material = material;
+    }
+
+    /// <inheritdoc/>
+    public void Execute()
+    {
+        // Get the index of resources managed by the current thread
+        // in both 2D textures and flat buffers
+        int2 index2D = ThreadIds.XY;
+        int fIndex = (index2D.Y * DispatchSize.X) + index2D.X;
+        
+        var cast = rayCastBuffer[fIndex];
+        var ray = rayBuffer[fIndex];
+
+        // If this material was not hit do not execute
+        if (cast.matId != matId)
+            return;
+
+        // Calculate diffuse and specular intensity
+        float4 diffuseIntensity = float4.Zero;
+        float4 specularIntensity = float4.Zero;
+        for (int i = 0; i < lightsBuffer.Length; i++)
+        {
+            var fShadowIndex = (i * DispatchSize.X * DispatchSize.Y) + (index2D.Y * DispatchSize.X) + index2D.X; 
+            var l = shadowCastBuffer[fShadowIndex].direction;
+            if (Hlsl.Length(l) == 0)
+                continue;
+            
+            var n = cast.smoothNormal;
+            var v = ray.direction;
+            var r = Hlsl.Reflect(l, cast.smoothNormal);
+            
+            diffuseIntensity += lightsBuffer[i].color * Hlsl.Max(Hlsl.Dot(n, l), 0f);
+            specularIntensity += lightsBuffer[i].color * Hlsl.Pow(Hlsl.Max(Hlsl.Dot(r, ray.direction), 0), material.roughness);
+        }
+
+        // Evaluate texture
+        float3 sines = Hlsl.Sin(cast.position * material.scale);
+        float sign = sines.X * sines.Z;
+        float4 diffuse = sign < 0 ? material.diffuse0 : material.diffuse1;
+
+        // Sum ambient, diffuse, and specular components
+        colorBuffer[index2D] += material.ambient;
+        colorBuffer[index2D] += diffuse * diffuseIntensity;
+        colorBuffer[index2D] += material.specular * specularIntensity;
+    }
+
+    ReadOnlyBuffer<Light> IMaterialShader.LightBuffer  { set => lightsBuffer = value; }
+
+    ReadWriteBuffer<Ray> IMaterialShader.RayBuffer { set => rayBuffer = value; }
+
+    ReadWriteBuffer<Ray> IMaterialShader.ShadowCastBuffer { set => shadowCastBuffer = value; }
+
+    ReadWriteBuffer<GeometryCollision> IMaterialShader.RayCastBuffer {  set => rayCastBuffer = value; }
+
+    IReadWriteNormalizedTexture2D<float4> IMaterialShader.AttenuationBuffer { set => _ = value; }
+
+    IReadWriteNormalizedTexture2D<float4> IMaterialShader.ColorBuffer { set => colorBuffer = value; }
+}
